16537f2c3e69219ed9fb1c811d84779a7cec3446
s31354 <s31354@pjwstk.edu.pl>
2025-06-10 09:57:21 +0200

    Remove repository folder

--- APBD/Test_2/Test_2/Infrastructure/InfrastructureServicesExtension.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Test_2.Infrastructure;
-
-public static class InfrastructureServicesExtension
-{
-    public static void RegisterInfrastructureServices(this IServiceCollection app)
-    {
-        // app.AddScoped<>();
-    }
-}
